cd558aea9337169db04dc755627c0f0cd232bd51
Jeff Wright <jeff.washcloth@gmail.com>
2016-02-09 12:34:03 -0500

    CSV file now contains date/time & ping stats.

--- linkCheck.py
+++ linkCheck.py
@@ -28,7 +28,7 @@ __version__ = "0.5"
 # GLOBAL EXCUTION VARS
 num_pings = '2'
 logfilename = 'modemtestreport.csv'
-csv_header = "Time,Ping Min,Ping Max,Ping Average,Upload Speed(KBps),Download Speed(KBps),Modem ID,Group,MDN,Carrier,IMEI,RSSI,RSRP,RSRQ,SINR,Firmware"
+csv_header = "Time,Ping Min,Ping Avg,Ping Max,Ping Dev,Upload Speed(KBps),Download Speed(KBps),Modem ID,Group,MDN,Carrier,IMEI,RSSI,RSRP,RSRQ,SINR,Firmware"
 
 import sys, os, socket, traceback, re, subprocess, datetime
 from ftplib import FTP
@@ -41,8 +41,15 @@ def main (arguments):
 
 
 def runIt(arguments):
-    print "Entering function 'pingFTP'"
+    """
+    Run following in order:
+      - ping FTP server <num_pings> times, & write results to file
+      - push upload to ftp server & write speed to file
+      - fetch download from ftp server & write speed to file
+      - get modem stats and write to file
+    """
 
+    # Ping FTP server
     try:
         ping = subprocess.Popen(["ping", "-c", num_pings,
                                  arguments['<ftp_server_ip>']],
@@ -50,35 +57,43 @@ def runIt(arguments):
                                  stderr = subprocess.PIPE)
         out, error = ping.communicate()
         matcher = re.compile("rtt min/avg/max/mdev = (\d+.\d+)/(\d+.\d+)/(\d+.\d+)/(\d+.\d+)")
+
     except socket.error, e:
         print "Ping Error:", e
         raise e
 
-    print "Exiting function 'pingFTP'"
     return matcher.search(out).groups()
 
 
 def logIt(data):
-    print "Entering function 'logIt'"
     
-    print "  data: ", data
+    print "  data: ", data, type(data)
 
     try:
         with open('modemtestreport.csv', 'a') as file:
             # If file is size zero, write header into it
-            info = os.stat(logfilename)
-            print "  info: ", info
-            print "  size: ", info.st_size
-            
-            if info.st_size == 0:
+            if os.stat(logfilename).st_size == 0:
                 file.write(csv_header)
-            now = datetime.datetime.now()
-            print "  now: ", now
-    except IOError as e:
-        print "Unable to open file"
-        raise e
 
-    print "Exiting function 'logIt'"
+            # Add date/timestamp
+            now = datetime.datetime.now()
+            try:
+                file.write('\n' + str(now) + ',')
+            except IOError as e:
+                print "Unable to write date-time to file."
+                raise e
+
+            # Add ping results
+            for item in data:
+                try:
+                    file.write(item + ',')
+                except IOError as e:
+                    print "Unable to write data to file."
+                    raise e
+
+    except IOError as e:
+        print "Unable to open file."
+        raise e
 
 
 if __name__ == '__main__':
