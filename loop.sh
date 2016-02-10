9cea5d0d11942a5680470f5f8c1cd41939cd3b54
Jeff Wright <jeff.washcloth@gmail.com>
2016-02-10 07:35:22 -0500

    Remove info from loop.sh

--- loop.sh
+++ loop.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-num_loops=5
+num_loops=5000
 sleep_time=2
 
 for i in `seq $num_loops`
 do
     printf 'Iteration %s\n' $i
     printf '============\n'
-    python linkCheck.py 66.82.228.131 ***** *****;
+    python linkCheck.py 66.82.228.131 npsrt H2ghes01
     if [ "$i" -lt "$num_loops" ]; then
         sleep $sleep_time
     fi
